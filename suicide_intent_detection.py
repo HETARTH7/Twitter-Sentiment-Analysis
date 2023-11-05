305d6ded1c74c1b8419f52592b38e4e060b27e7c
unknown <hetarth002@gmail.com>
2023-11-05 10:11:42 +0530

    modify testing data

--- suicide_intent_detection.py
+++ suicide_intent_detection.py
@@ -16,25 +16,26 @@ import numpy as np
 happy = pd.read_csv('happy.csv')
 emotions = pd.read_csv('tweet_emotions.csv')
 suicide = pd.read_csv('Suicide_Detection.csv')
+
 data = []
 
 happy_data = happy[['cleaned_hm']].rename(
-    columns={'cleaned_hm': 'text'}).head(5)
+    columns={'cleaned_hm': 'text'}).head(100)
 happy_data['class'] = 'Happy Intent'
 data.append(happy_data)
 
 sad_emotions_data = emotions[emotions['sentiment'] == 'sadness'][[
-    'content']].rename(columns={'content': 'text'}).head(5)
+    'content']].rename(columns={'content': 'text'}).head(100)
 sad_emotions_data['class'] = 'Sad Intent'
 data.append(sad_emotions_data)
 
 neutral_emotions_data = emotions[emotions['sentiment'].isin(
-    ['empty', 'neutral'])][['content']].rename(columns={'content': 'text'}).head(5)
+    ['empty', 'neutral'])][['content']].rename(columns={'content': 'text'}).head(100)
 neutral_emotions_data['class'] = 'Normal Intent'
 data.append(neutral_emotions_data)
 
 suicide_data = suicide[suicide['class'] == 'suicide'][[
-    'text']].rename(columns={'text': 'text'}).head(5)
+    'text']].rename(columns={'text': 'text'}).head(100)
 suicide_data['class'] = 'Suicide Intent'
 data.append(suicide_data)
 
@@ -49,7 +50,7 @@ print(class_counts)
 
 # nltk.download('stopwords')
 corpus = []
-for i in range(0, 20):
+for i in range(0, 400):
     review = re.sub('[^a-zA-Z]', ' ', dataset['text'][i])
     review = review.lower()
     review = review.split()
@@ -61,30 +62,32 @@ for i in range(0, 20):
     review = ' '.join(review)
     corpus.append(review)
 
-
 print(corpus)
 
 cv = CountVectorizer()
+x = np.array(corpus)
 X = cv.fit_transform(corpus).toarray()
 y = dataset['class'].values
 
-X_train, X_test, y_train, y_test = train_test_split(
+X_train, X_test, Y_train, Y_test = train_test_split(
     X, y, test_size=0.2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=0)
 
 classifier = GaussianNB()
-classifier.fit(X_train, y_train)
+classifier.fit(X_train, Y_train)
 
 classifier = SVC(kernel='linear', random_state=0)
-classifier.fit(X_train, y_train)
+classifier.fit(X_train, Y_train)
 
 y_pred = classifier.predict(X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), Y_test.reshape(len(Y_test), 1)), 1))
 
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(Y_test, y_pred)
 print(cm)
-print(f'Accuracy:', accuracy_score(y_test, y_pred)*100, '%')
+print(f'Accuracy:', accuracy_score(Y_test, y_pred)*100, '%')
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/HETARTH RAVAL/Desktop/AI/Machine Learning/projects/suicide-intent-detection-d40b46ea43fa.json"
 
 
 def detect_intent(text, project_id, session_id, language_code):
@@ -107,11 +110,9 @@ project_id = "suicide-intent-detection"
 session_id = str(uuid.uuid4())
 language_code = "en"
 
-corpus = np.array(corpus)
-
 predicted_intents = []
-for i in range(0, 20):
-    text = corpus[i]
+for i in range(0, 80):
+    text = x_test[i]
     text_chunks = [text[i:i+256] for i in range(0, len(text), 256)]
     predicted_intents_for_text = []
 
@@ -123,5 +124,6 @@ for i in range(0, 20):
     predicted_intents.append(combined_intent)
 
 predicted_intents = np.array(predicted_intents)
+predicted_intents.size
 
-print(f'Accuracy:', accuracy_score(y, predicted_intents)*100, '%')
+print(f'Accuracy:', accuracy_score(y_test, predicted_intents)*100, '%')
